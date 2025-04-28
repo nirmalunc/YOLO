7508967b5ec4d8f58d22ec17bd290749c4176d09
LemunadeStand <neptommy@gmail.com>
2025-04-28 02:58:07 -0400

    Working before long run

--- ultralytics/test_bifpn.py
+++ ultralytics/test_bifpn.py
@@ -1,5 +1,10 @@
 from ultralytics import YOLO
 
+import torch
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0))
+
 model = YOLO('yolov8_bifpn.yaml')
 
-results = model.train(data="datasets/coco128.yaml", epochs=2, imgsz=640, device="mps")
+results = model.train(data="datasets/coco.yaml", epochs=25, imgsz=640, device=0, batch=0.9, workers=16)
